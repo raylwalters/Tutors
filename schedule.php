68110dc9fffb4bf601b68f5d1453a83e6ec1fc73
Ray Walters <rlwalters3@yahoo.com>
2017-07-03 14:05:35 -0400

    Update schedule page.

--- schedule.php
+++ schedule.php
@@ -15,27 +15,76 @@
 	<?php 
 		include 'header.php';
 	?>
+	<div class="content">
+	<div class="col-xs-12" id="top-logo">
+		<img src="schedule.jpg" alt='schedule'>
+	</div>
 	<div class="row">
-		<div class="col-xs-4">
+		<div class="col-xs-4 schedule heading">
 			<h3>Day</h3>
 		</div>
-		<div class="col-xs-4">
+		<div class="col-xs-4 schedule heading">
 			<h3>Location</h3>
 		</div>
-		<div class="col-xs-4">
+		<div class="col-xs-4 schedule heading">
 			<h3>Time</h3>
 		</div>
 	</div>
 	<div class="row">
-		<div class="col-xs-4">
-			<p>Monday</p>
+		<div class="col-xs-4 schedule">
+			<p class='day'>Monday</p>
 		</div>
-		<div class="col-xs-4">
+		<div class="col-xs-4 schedule">
 			<p>Southfield Public Library</p>
 		</div>
-		<div class="col-xs-4">
-			<p>3:30 pm - 8:30 pm</p>
+		<div class="col-xs-4 schedule">
+			<p class='time'>3:30 pm - 8:30 pm</p>
 		</div>
+	</div>
+	<div class="row">
+		<div class="col-xs-4 schedule">
+			<p class='day'>Tuesday</p>
+		</div>
+		<div class="col-xs-4 schedule">
+			<p>Baldwin Public Library, Birmingham</p>
+		</div>
+		<div class="col-xs-4 schedule">
+			<p class='time'>3:30 pm - 8:30 pm</p>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-xs-4 schedule">
+			<p class='day'>Wednesday</p>
+		</div>
+		<div class="col-xs-4 schedule">
+			<p>Baldwin Public Library, Birmingham</p>
+		</div>
+		<div class="col-xs-4 schedule">
+			<p class='time'>3:30 pm - 8:30 pm</p>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-xs-4 schedule">
+			<p class='day'>Thursday</p>
+		</div>
+		<div class="col-xs-4 schedule">
+			<p>Bloomfield Township Public Library</p>
+		</div>
+		<div class="col-xs-4 schedule">
+			<p class='time'>3:30 pm - 8:30 pm</p>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-xs-4 schedule">
+			<p class='day'>Friday</p>
+		</div>
+		<div class="col-xs-4 schedule">
+			<p>Bloomfield Township Public Library</p>
+		</div>
+		<div class="col-xs-4 schedule">
+			<p class='time'>3:00 pm - 6:00 pm</p>
+		</div>
+	</div>
 	</div>
 	
 
